940772d29199dfc06e168066f756876e09037885
aadit6 <nagpalaadit@gmail.com>
2023-12-03 15:07:14 +0000

    3/12

--- server.js
+++ server.js
@@ -1,42 +1,61 @@
 // imports and constants
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt"); //should i use this or crypto - scrypt builtin module?
+const bcrypt = require("bcrypt"); 
 const database = require("./database");
-
-
+const middleware = require("./middleware");
+const ejs = require("ejs")
+const bodyParser = require("body-parser");
+const path = require("path");
 
 
 // initialising server (mounting middleware)
 const port = 3000;
-app.use(express.urlencoded({extended: true})); //middleware to parse form
-app.use("/src", express.static("frontend/src")); //middleware to serve static js files??
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(middleware.logPaths);
+app.use(bodyParser.urlencoded({extended: true})); //middleware to parse form
 app.use(express.json()); //allows to use json format => maybe hardcode ourselves for +complexity though?
-function logger(req, res, next) { //custom middleware to log requests in console
-    console.log("Request Method:", req.method)
-    console.log("Request URL:", req.urlencoded)
-    next()
-}
-app.use(logger); //maybe later at some point have this in the middleware.js file
+app.use(middleware.logger); //maybe later at some point have this in the middleware.js file
+app.set('view engine', 'ejs')
 
-//add sessions etc. later
+// add sessions etc. later
 
 app.get('/', (req, res)=>{
-    res.render(__dirname + "/frontend/public/login.html", {error:"", success:""})
+    res.render(__dirname + "/views/settings.ejs", {error:"", success:""})
 })
 
-app.post('/loginsubmit',(req,res) => {
+
+
+app.post('/registersubmit',(req,res) => { //unfinished: need to finish DB first.
+    const pwd = req.body.pwd;
+    const username = req.body.username
+    const email = req.body.email
+
+    //hashing
+    const saltRounds = 15;
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(pwd, salt, function(err, hash) {
+            // Store hash in sql database
+            if(err) { 
+                console.log("ERROR: hashing error")
+                console.log(err)
+                res.render(__dirname + "/views/register.ejs", {errMsg:"Error while hashing. Please try again.",successMsg:""});
+                return;
+            }
+
+        });
+    });
+})
+
+app.post('/loginsubmit',(req,res) =>{
     
 })
 
-app.post('/registersubmit',(req,res) =>{
-    
-})
 
 
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}.`);
+    console.log(`server running on port ${port} and path ${__dirname}.`);
     
 })
 
